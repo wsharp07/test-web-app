1738db6f484354704dff85e950be962ea25b9ca8
William Sharp <wsharp07@gmail.com>
2018-05-14 21:47:03 -0400

    initial push

---
+++ TestWebApp/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using TestWebApp.Models.User;
+
+namespace TestWebApp.Controllers
+{
+    public class UserController : AsyncController
+    {
+        private static readonly HttpClient _client = new HttpClient();
+        // GET: User
+        public async Task<ActionResult> Index()
+        {
+            UserViewModel vm = new UserViewModel();
+            HttpRequestMessage request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://reqres.in/api/users")
+            };
+
+            var response = await _client.SendAsync(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                vm.UserResponse = data;
+            }
+ 
+            return View(vm);
+        }
+    }
+}
---
+++ TestWebApp/Models/User/UserViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWebApp.Models.User
+{
+    public class UserViewModel
+    {
+        public string UserResponse { get; set; }
+    }
+}
